d298fc95a22f3f7c270a32a63796f1847bff2430
juno <papaci2222@gmail.com>
2024-01-18 16:48:54 +0900

    240118_TIL

--- git/baekjoon/15664(미해결).py
+++ git/baekjoon/15664(미해결).py
@@ -4,14 +4,14 @@ def seq(N, M, level=0, state=[]):
         return
 
     for i in range(N-level):
-        if sample[i-1] == sample[i]: continue
+        if sample[i-1] != sample[i]:
         # if state is None or sample[i] <= state[0]: continue
-        state.append(sample[i])
-        sample.pop(i)
-        seq(N, M, level + 1, state)
-        sample.append(state[level])
-        sample.sort()
-        state.pop()
+            state.append(sample[i])
+            sample.pop(i)
+            seq(N, M, level + 1, state)
+            sample.append(state[level])
+            sample.sort()
+            state.pop()
 
 N, M = map(int, input().split())
 sample = list(map(int, input("").split()))
