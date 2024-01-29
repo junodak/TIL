2847057482ab269cb4bd0578fbff52efae12b2e7
juno <papaci2222@gmail.com>
2024-01-29 17:57:54 +0900

    240129_baekjoon

---
+++ git/baekjoon/12865.py
@@ -0,0 +1,21 @@
+n, k = map(int, input().split())
+w = [0]*n
+v = [0]*n
+dp = [0]*n
+
+for i in range(n):
+    w[i], v[i] = map(int, input().split())
+
+# 4 7   
+# 6 13  0
+# 4 8   0
+# 3 6   0
+# 5 12  0
+    
+
+# 7
+
+# 3 6   0
+# 4 8   0
+# 5 12  0
+# 6 13  0
