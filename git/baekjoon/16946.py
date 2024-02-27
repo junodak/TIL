4b02ea518d07519724b30a2560cb7c497e5264fb
junodak <papaci2222@gmail.com>
2024-02-28 08:32:35 +0900

    240228_baekjoon_1

--- git/baekjoon/16946.py
+++ git/baekjoon/16946.py
@@ -1,54 +1,48 @@
 from collections import deque
-n, m = map(int, input().split())
-arr = [list(map(int, list(input().strip()))) for _ in range(n)]
 
 dn = [1, -1, 0, 0]
 dm = [0, 0, 1, -1]
 
-zero = set()
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+    visited[x][y] = group_num
+    cnt = 0
+    while q:
+        qi, qj = q.popleft()
+        cnt += 1
+        for k in range(4):
+            di = qi + dn[k]
+            dj = qj + dm[k]
+            if 0 <= di < n and 0 <= dj < m:
+                if visited[di][dj] == 0 and arr[di][dj] == 0:
+                    q.append((di, dj))
+                    visited[di][dj] = group_num
+    group_cnt.append(cnt)
 
 
+n, m = map(int, input().split())
+arr = [list(map(int, list(input().strip()))) for _ in range(n)]
+visited = [[0]*m for _ in range(n)]
+group_cnt = [0]
+group_num = 0
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 0 and visited[i][j] == 0:
+            group_num += 1
+            bfs(i, j)
 
 for i in range(n):
     for j in range(m):
-        if arr[i][j] == 0:
-            
-
-
-
-
-# from collections import deque
-# import sys
-# input = sys.stdin.readline
-
-# n, m = map(int, input().split())
-# arr = [list(map(int, list(input().strip()))) for _ in range(n)]
-
-# dn = [1, -1, 0, 0]
-# dm = [0, 0, 1, -1]
-
-# def bfs(i, j):
-#     q = deque()
-#     q.append((i, j))
-#     visited = [[0]*m for _ in range(n)]
-#     visited[i][j] = 1
-#     cnt = 1
-#     while q:
-#         qi, qj = q.popleft()
-#         for k in range(4):
-#             di = qi + dn[k]
-#             dj = qj + dm[k]
-#             if 0 <= di < n and 0 <= dj < m:
-#                 if arr[di][dj] == 0 and visited[di][dj] == 0:
-#                     cnt += 1
-#                     q.append((di, dj))
-#                     visited[di][dj] = 1
-#     return cnt%10
-
-# path = []
-# for i in range(n):
-#     for j in range(m):
-#         if arr[i][j]:
-#             arr[i][j] = bfs(i, j)
-#         print(arr[i][j], end='')
-#     print()
+        if arr[i][j]:
+            a = set()
+            for k in range(4):
+                dx = i + dn[k]
+                dy = j + dm[k]
+                if 0 <= dx < n and 0 <= dy < m:
+                    a.add(visited[dx][dy])
+            for k in a:
+                arr[i][j] += group_cnt[k]
+            arr[i][j] %= 10
+        print(arr[i][j], end='')
+    print()
