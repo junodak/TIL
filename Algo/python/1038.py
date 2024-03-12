9382fdfa8d006f7e8544bd2b3ec9375abdec9bc9
Minjun-shin <minjun.shin1201@gmail.com>
2024-03-12 17:55:31 +0900

    240312_baekjoon_1

--- Algo/python/1038.py
+++ Algo/python/1038.py
@@ -1,38 +1,69 @@
-n = int(input())
+arr = list(range(0, 10))
+for i in range(1 << 10):
+    for j in range(10):
+        if i & (1 << j):
+            print(arr[j], end='')
+    print()
 
-check = [0, 9, 45, 120, 210, 252, 210, 120, 45, 10, 1]
 
-map = [
-[1, 1, 1, 1, 1, 1, 1, 1, 1],
-[1, 2, 3, 4, 5, 6, 7, 8, 9],
-[0, 1, 3, 6, 10, 15, 21, 28, 36],
-[0, 0, 1, 4, 10, 20, 35, 56, 84],
-[0, 0, 0, 1, 5, 15, 35, 70, 126],
-[0, 0, 0, 0, 1, 6, 21, 56, 126],
-[0, 0, 0, 0, 0, 1, 7, 28, 84],
-[0, 0, 0, 0, 0, 0, 0, 1, 8, 36],
-[0, 0, 0, 0, 0, 0, 0, 1, 9],
-[0, 0, 0, 0, 0, 0, 0, 0, 1]
-]
 
-for i in range(1,11):
-    if sum(check[:i]) < n and  n <= sum(check[:i+1]):
-        print(map[i-1])
-        n -= sum(check[:i])
-        print('남은거 :',n)
+# n = int(input())
+# res = 0
+# for _ in range(n):
+#     res += 1
+#     while True:
+#         temp = str(res)
+#         len_temp = len(temp)
+#         check = False
+#         for i in range(len_temp):
+#             for j in range(i+1, len_temp):
+#                 if int(temp[i]) <= int(temp[j]):
+#                     check = True
+#         if check:
+#             res += 1
+#         else:
+#             break
+# print(res)
 
-        for j in range(10):
-            if n>map[i-1][j]:
-                n-=map[i-1][j]
-            else:
-                print(j+1, end='')
-                for k in range(10):
-                    if n>map[i-2][k]:
-                        n-=map[i-2][k]
-                    else:
-                        print(k+1, n-1)
-                        break
-                break
+
+
+
+
+# n = int(input())
+#
+# check = [0, 9, 45, 120, 210, 252, 210, 120, 45, 10, 1]
+#
+# map = [
+# [1, 1, 1, 1, 1, 1, 1, 1, 1],
+# [1, 2, 3, 4, 5, 6, 7, 8, 9],
+# [0, 1, 3, 6, 10, 15, 21, 28, 36],
+# [0, 0, 1, 4, 10, 20, 35, 56, 84],
+# [0, 0, 0, 1, 5, 15, 35, 70, 126],
+# [0, 0, 0, 0, 1, 6, 21, 56, 126],
+# [0, 0, 0, 0, 0, 1, 7, 28, 84],
+# [0, 0, 0, 0, 0, 0, 0, 1, 8, 36],
+# [0, 0, 0, 0, 0, 0, 0, 1, 9],
+# [0, 0, 0, 0, 0, 0, 0, 0, 1]
+# ]
+#
+# for i in range(1,11):
+#     if sum(check[:i]) < n and  n <= sum(check[:i+1]):
+#         print(map[i-1])
+#         n -= sum(check[:i])
+#         print('남은거 :',n)
+#
+#         for j in range(10):
+#             if n>map[i-1][j]:
+#                 n-=map[i-1][j]
+#             else:
+#                 print(j+1, end='')
+#                 for k in range(10):
+#                     if n>map[i-2][k]:
+#                         n-=map[i-2][k]
+#                     else:
+#                         print(k+1, n-1)
+#                         break
+#                 break
                 
 
 
